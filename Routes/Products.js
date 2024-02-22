2e7fff0ae8d6eaaf93d82384bdbca0335eee0eb0
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2024-02-22 10:40:31 +0530

    feat

--- Routes/Products.js
+++ Routes/Products.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {
   getAllProducts,
   getAllProductsStatic,
-} = require("../Controllers/Product");
+} = require("../controllers/products");
 
 router.route("/").get(getAllProducts);
 router.route("/static").get(getAllProductsStatic);
